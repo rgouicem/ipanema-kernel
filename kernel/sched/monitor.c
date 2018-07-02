234baf569fc91ee4b9a91199c26a7bdda3a7313b
Paul-Nicolas Madelaine <paul-nicolas.madelaine@ens.fr>
2018-07-02 17:44:20 +0200

    tracer_seq_show: added tick_evt

--- kernel/sched/monitor.c
+++ kernel/sched/monitor.c
@@ -334,6 +334,7 @@ static int tracer_seq_show(struct seq_file *s, void *v)
 	case FORK_EVT:
 	case MIGRATE_EVT:
 	case RQ_SIZE:
+	case TICK_EVT:
 		seq_printf(s, "%llu %s %d %d %d\n",
 			   evt->timestamp, sched_tracer_events_str[evt->event],
 			   evt->pid, evt->arg0, evt->arg1);
